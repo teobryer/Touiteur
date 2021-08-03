3e8f7bb089f74ae5c615bab7311361d8de381db8
teobryer <teocalme@gmail.com>
2021-08-03 12:05:54 +0200

    Navbar & logo

---
+++ frames/bottom.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Touiteur</strong> by <a href="https://www.linkedin.com/in/téo-bryer-035648153/?originalSubdomain=fr">Téo Bryer</a>
+    </p>
+  </div>
+</footer>
+
+
+</body>
+</html>
---
+++ frames/top.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma 2 !</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+  <nav class="level" style=" margin-top: 10px";>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Touit' List</a>
+  </p>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Mon profil</a>
+  </p>
+  <p class="level-item has-text-centered">
+    <img src="img/logo.png" alt="" style="height: 80px;">
+  </p>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Paramètres</a>
+  </p>
+  <p class="level-item has-text-centered">
+    <a class="link is-info">Se déconnecter</a>
+  </p>
+</nav>
